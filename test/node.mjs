a179babf9c06302e0de8dcfadc242aecc1a6466b
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-01-17 13:45:43 +0100

    Implement noncustodial `createServer()`

--- test/node.mjs
+++ test/node.mjs
@@ -107,3 +107,37 @@ test('server mode', (t) =>
     await Promise.all([connect, io]).then(() => server.close())
   })))
 )
+
+test('noncustodial server mode', (t) =>
+  withDHT((dht) => withRelay(dht, (relay) => withNode(relay, { custodial: false }, async (node) => {
+    const connect = t.test('connect')
+    connect.plan(3)
+
+    const io = t.test('read and write')
+    io.plan(2)
+
+    const server = node.createServer()
+    await server.listen()
+
+    const socket = dht.connect(server.address().publicKey)
+
+    socket.on('open', () => {
+      connect.pass('client connected')
+      connect.alike(socket.remotePublicKey, server.address().publicKey)
+
+      socket.write('ping')
+      socket.once('data', (data) => io.alike(data.toString(), 'pong'))
+    })
+
+    server.on('connection', (socket) => {
+      connect.pass('server connected')
+
+      socket.on('data', (data) => {
+        io.alike(data.toString(), 'ping')
+        socket.write('pong')
+      })
+    })
+
+    await Promise.all([connect, io]).then(() => server.close())
+  })))
+)
