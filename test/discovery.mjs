b26151c047d4c9573213adeb4657c2372eccc9e3
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-01-19 15:20:39 +0100

    More discovery tests and fixes

--- test/discovery.mjs
+++ test/discovery.mjs
@@ -4,14 +4,16 @@ import { withDHT } from './helpers/with-dht.mjs'
 import { withRelay } from './helpers/with-relay.mjs'
 import { withNode } from './helpers/with-node.mjs'
 
-test('announce', (t) =>
+test('lookup', (t) =>
   withDHT((dht) => withRelay(dht, (relay) => withNode(relay, async (node) => {
+    t.plan(3)
+
     const topic = Buffer.alloc(32, 'topic')
     const keyPair = dht.constructor.keyPair()
 
     await dht.announce(topic, keyPair).finished()
 
-    const query = node.announce(topic, node.defaultKeyPair)
+    const query = node.lookup(topic)
 
     for await (const result of query) {
       t.alike(result.peers, [
@@ -24,22 +26,72 @@ test('announce', (t) =>
   })))
 )
 
-test('noncustodial announce', (t) =>
-  withDHT((dht) => withRelay(dht, (relay) => withNode(relay, { custodial: false }, async (node) => {
+test('announce', (t) =>
+  withDHT((dht) => withRelay(dht, (relay) => withNode(relay, async (node) => {
+    t.plan(3)
+
     const topic = Buffer.alloc(32, 'topic')
-    const keyPair = dht.constructor.keyPair()
 
-    await dht.announce(topic, keyPair).finished()
+    await node.announce(topic, node.defaultKeyPair).finished()
 
-    const query = node.announce(topic, node.defaultKeyPair)
-
-    for await (const result of query) {
+    for await (const result of dht.lookup(topic)) {
       t.alike(result.peers, [
         {
-          publicKey: keyPair.publicKey,
+          publicKey: node.defaultKeyPair.publicKey,
           relayAddresses: []
         }
       ])
     }
+  })))
+)
+
+test('noncustodial announce', (t) =>
+  withDHT((dht) => withRelay(dht, (relay) => withNode(relay, { custodial: false }, async (node) => {
+    t.plan(3)
+
+    const topic = Buffer.alloc(32, 'topic')
+
+    await node.announce(topic, node.defaultKeyPair).finished()
+
+    for await (const result of dht.lookup(topic)) {
+      t.alike(result.peers, [
+        {
+          publicKey: node.defaultKeyPair.publicKey,
+          relayAddresses: []
+        }
+      ])
+    }
+  })))
+)
+
+test('unannounce', (t) =>
+  withDHT((dht) => withRelay(dht, (relay) => withNode(relay, async (node) => {
+    const topic = Buffer.alloc(32, 'topic')
+
+    await dht.announce(topic, node.defaultKeyPair).finished()
+
+    await node.unannounce(topic, node.defaultKeyPair)
+
+    for await (const result of dht.lookup(topic)) {
+      t.absent(result)
+    }
+
+    t.pass()
+  })))
+)
+
+test('noncustodial unannounce', (t) =>
+  withDHT((dht) => withRelay(dht, (relay) => withNode(relay, { custodial: false }, async (node) => {
+    const topic = Buffer.alloc(32, 'topic')
+
+    await dht.announce(topic, node.defaultKeyPair).finished()
+
+    await node.unannounce(topic, node.defaultKeyPair)
+
+    for await (const result of dht.lookup(topic)) {
+      t.absent(result)
+    }
+
+    t.pass()
   })))
 )
